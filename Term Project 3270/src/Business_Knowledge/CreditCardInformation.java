aac3a603862b1fd59b441c1a1b7fe8f5177e907b
BryantVarner <47334002+BryantVarner@users.noreply.github.com>
2019-03-26 21:47:56 -0400

    updated customer class

--- Term Project 3270/src/Business_Knowledge/CreditCardInformation.java
+++ Term Project 3270/src/Business_Knowledge/CreditCardInformation.java
@@ -1,5 +1,11 @@
 package Business_Knowledge;
 
 public class CreditCardInformation {
+	
+	private String CardType;
+	private String NameOnCard;
+	private String CardNumb;
+	private String ExpirationDate;
+	
 
 }
--- Term Project 3270/src/Business_Knowledge/Customer.java
+++ Term Project 3270/src/Business_Knowledge/Customer.java
@@ -5,12 +5,80 @@ public class Customer  {
 	private String firstName;
 	private String lastName;
 	private String address;
-	private int zipcode;
+	private int zipCode;
 	private String state;
 	private String username;
 	private String password;
 	private String email;
 	private String SSN;
 	private String securityAnswer;
+	
+	
+	
+	public Customer() {
+		
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getZipcode() {
+		return zipCode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipCode = zipcode;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSSN() {
+		return SSN;
+	}
+	public void setSSN(String sSN) {
+		SSN = sSN;
+	}
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
 
 }
