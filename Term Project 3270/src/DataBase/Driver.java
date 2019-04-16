0b212ce40b6e5256bd35057968cd7487172ba461
BryantVarner <47334002+BryantVarner@users.noreply.github.com>
2019-04-16 14:52:07 -0400

    connected database and tested with success

--- Term Project 3270/src/DataBase/Driver.java
+++ Term Project 3270/src/DataBase/Driver.java
@@ -4,37 +4,30 @@ import java.sql.*;
 
 public class Driver {
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException  {
 		
-			    // Load the JDBC driver
-			    System.out.println("Driver loaded");
 			
-			    // Establish a connection
-			    Connection connection = DriverManager.getConnection
-			      ("jdbc:mysql://localhost/test");
-			    System.out.println("Database connected");
-			
-			    
-			    String queryString = "select firstName, mi, " + 
-			    "lastName , title, grade from Student, Enrollment, Course " +
-			    "where Student.ssn = ? and Enrollment.courseId = ? " +
-			    "and Enrollment.courseId = Course.courseId" ;
-			    
-			    // Create a statement
-			  //   preparedStatement = connection.preparedStatement(queryString);
-			
-			    // Execute a statement
-			//    ResultSet resultSet = statement.executeQuery
-			     // ("select firstName, mi, lastName from Student where lastName "
-			  //      + " = 'Smith'");
-			
-			    // Iterate through the result and print the student names
-			 //   while (resultSet.next())
-			 //     System.out.println(resultSet.getString(1) + "\t" +
-			 //       resultSet.getString(2) + "\t" + resultSet.getString(3));
-			
-			    // Close the connection
-			    connection.close();
-			  }
+			// Establish a connection
+		    Connection connection = DriverManager.getConnection
+		      ("jdbc:mysql://localhost/airlinereservation" , "root" , "nodummies12345");
+		    System.out.println("Database connected");
+		
+		    // Create a statement
+		    Statement statement = connection.createStatement();
+		
+		    // Execute a statement
+		    ResultSet resultSet = statement.executeQuery
+		      ("select * from customer");
+		
+		    // Iterate through the result and print the student names
+		    while (resultSet.next())
+		      System.out.println(resultSet.getString("name") + " , " +
+		        resultSet.getString("address"));
+		
+		    // Close the connection
+		    connection.close();
+		
+	}
+			  
 
 }
