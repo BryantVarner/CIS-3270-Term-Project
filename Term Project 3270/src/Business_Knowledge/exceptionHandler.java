90ff7962d54d08e00ce3158cd4174a62b4b3e251
BryantVarner <47334002+BryantVarner@users.noreply.github.com>
2019-04-15 23:29:03 -0400

    added driver class and exception handler class

--- Term Project 3270/src/Business_Knowledge/exceptionHandler.java
+++ Term Project 3270/src/Business_Knowledge/exceptionHandler.java
@@ -1,6 +1,6 @@
 package Business_Knowledge;
 
-public class exceptions {
+public class exceptionHandler {
 	
 	
 
--- Term Project 3270/src/DataBase/Driver.java
+++ Term Project 3270/src/DataBase/Driver.java
@@ -1,11 +1,40 @@
 package DataBase;
 
+import java.sql.*;
 
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		
-
-	}
+			    // Load the JDBC driver
+			    System.out.println("Driver loaded");
+			
+			    // Establish a connection
+			    Connection connection = DriverManager.getConnection
+			      ("jdbc:mysql://localhost/test");
+			    System.out.println("Database connected");
+			
+			    
+			    String queryString = "select firstName, mi, " + 
+			    "lastName , title, grade from Student, Enrollment, Course " +
+			    "where Student.ssn = ? and Enrollment.courseId = ? " +
+			    "and Enrollment.courseId = Course.courseId" ;
+			    
+			    // Create a statement
+			  //   preparedStatement = connection.preparedStatement(queryString);
+			
+			    // Execute a statement
+			//    ResultSet resultSet = statement.executeQuery
+			     // ("select firstName, mi, lastName from Student where lastName "
+			  //      + " = 'Smith'");
+			
+			    // Iterate through the result and print the student names
+			 //   while (resultSet.next())
+			 //     System.out.println(resultSet.getString(1) + "\t" +
+			 //       resultSet.getString(2) + "\t" + resultSet.getString(3));
+			
+			    // Close the connection
+			    connection.close();
+			  }
 
 }
